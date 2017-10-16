2bb3481ce31890ba20878738386eb8e224f8aa9e
Gregory Goijaerts <crecketgaming@gmail.com>
2017-10-17 00:29:30 +0200

    Added payment endpoint and fixed minor issues in monetary docs/response

--- src/Api/Payments.ts
+++ src/Api/Payments.ts
@@ -2,7 +2,7 @@ import ApiAdapter from "../ApiAdapter";
 import Session from "../Session";
 import ApiEndpointInterface from "../Interfaces/ApiEndpointInterface";
 
-export default class MonetaryAccount implements ApiEndpointInterface {
+export default class Payments implements ApiEndpointInterface {
     ApiAdapter: ApiAdapter;
     Session: Session;
 
@@ -15,30 +15,39 @@ export default class MonetaryAccount implements ApiEndpointInterface {
     }
 
     /**
-     *
+     * @param {number} userId
+     * @param {number} monetaryAccountId
+     * @param {number} paymentId
      * @param options
-     * @returns {Promise<any>}
+     * @returns {Promise<void>}
      */
     public async get(
+        userId: number,
+        monetaryAccountId: number,
+        paymentId: number,
+        options: any = {}
+    ) {
+        const response = await this.ApiAdapter.get(
+            `/v1/user/${userId}/monetary-account/${monetaryAccountId}/payment/${paymentId}`
+        );
+
+        // return raw respone image
+        return response.Response[0].Payment;
+    }
+
+    /**
+     * @param {number} userId
+     * @param {number} monetaryAccountId
+     * @param options
+     * @returns {Promise<void>}
+     */
+    public async list(
         userId: number,
         monetaryAccountId: number,
         options: any = {}
     ) {
         const response = await this.ApiAdapter.get(
-            `/v1/user/${userId}/monetary-account/${monetaryAccountId}`
-        );
-
-        // return raw respone image
-        return response.Response;
-    }
-    /**
-     *
-     * @param options
-     * @returns {Promise<any>}
-     */
-    public async list(userId: number, options: any = {}) {
-        const response = await this.ApiAdapter.get(
-            `/v1/user/${userId}/monetary-account`
+            `/v1/user/${userId}/monetary-account/${monetaryAccountId}/payment`
         );
 
         // return raw respone image
--- src/Api/index.ts
+++ src/Api/index.ts
@@ -3,6 +3,7 @@ import AttachementContent from "./AttachementContent";
 import DeviceRegistration from "./DeviceRegistration";
 import Installation from "./Installation";
 import MonetaryAccount from "./MonetaryAccount";
+import Payments from "./Payments";
 import SessionServer from "./SessionServer";
 
 export default (ApiAdapter: ApiAdapter) => {
@@ -11,6 +12,7 @@ export default (ApiAdapter: ApiAdapter) => {
         deviceRegistration: new DeviceRegistration(ApiAdapter),
         installation: new Installation(ApiAdapter),
         monetaryAccount: new MonetaryAccount(ApiAdapter),
+        payment: new Payments(ApiAdapter),
         sessionServer: new SessionServer(ApiAdapter),
     }
 }
