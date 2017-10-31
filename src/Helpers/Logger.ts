57492fdd06c5c6db093f7d701653313c9f415f6e
Gregory Goijaerts <crecketgaming@gmail.com>
2017-10-31 20:58:55 +0100

    Bugfix attempt where loglevel wasn't imported properly

--- src/Helpers/Logger.ts
+++ src/Helpers/Logger.ts
@@ -1,4 +1,6 @@
-import loglevel from "loglevel";
+declare const process: any;
+declare const require: any;
+const loglevel: any = require("loglevel");
 
 // set logging level based on env
 loglevel.setLevel(process.env.NODE_ENV === "development" ? "trace" : "warn");
