4286b36ba884c3a36e7b23d4ca2c08fbaffa5525
Gregory Goijaerts <crecketgaming@gmail.com>
2018-02-22 11:07:55 +0100

    Modified addresses to be optional

--- src/Api/RequestResponse.ts
+++ src/Api/RequestResponse.ts
@@ -119,8 +119,13 @@ export default class RequestResponse implements ApiEndpointInterface {
         // if request is accepted we set the shipping and amount details
         if (status === "ACCEPTED") {
             requestOptions.amount_responded = defaultOptions.amount_responded;
-            requestOptions.address_shipping = defaultOptions.address_shipping;
-            requestOptions.address_billing = defaultOptions.address_billing;
+            if (defaultOptions.address_shipping) {
+                requestOptions.address_shipping =
+                    defaultOptions.address_shipping;
+            }
+            if (defaultOptions.address_billing) {
+                requestOptions.address_billing = defaultOptions.address_billing;
+            }
         }
 
         const limiter = this.ApiAdapter.RequestLimitFactory.create(
