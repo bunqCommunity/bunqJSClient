78eb6955e9ff955a2c666ba6e20cf78f46f1c0a0
Ties <ties.schwertasek@gmail.com>
2018-12-05 20:44:44 +0100

    Remove dist files from repo

--- dist/Helpers/Logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const loglevel = require("loglevel");
-// set logging level based on env
-loglevel.setLevel(process.env.NODE_ENV === "development" ? "trace" : "warn");
-// export the logger
-exports.default = loglevel;
