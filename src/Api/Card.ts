046ca2bd73a905c05a4c5ae3e7d87a1cb8145b8c
lalilaloe <jonathan@superbuddy.nl>
2019-02-04 14:02:37 +0100

    Some comment additions with test findings

--- src/Api/Card.ts
+++ src/Api/Card.ts
@@ -9,71 +9,71 @@ import Amount from "../Types/Amount";
 import MagStripePermission from "../Types/MagStripePermission";
 
 export default class Card implements ApiEndpointInterface {
-    ApiAdapter: ApiAdapter;
-    Session: Session;
+	ApiAdapter: ApiAdapter;
+	Session: Session;
 
     /**
      * @param {ApiAdapter} ApiAdapter
      */
-    constructor(ApiAdapter: ApiAdapter) {
-        this.ApiAdapter = ApiAdapter;
-        this.Session = ApiAdapter.Session;
-    }
+	constructor(ApiAdapter: ApiAdapter) {
+		this.ApiAdapter = ApiAdapter;
+		this.Session = ApiAdapter.Session;
+	}
 
     /**
      *
      * @param options
      * @returns {Promise<any>}
      */
-    public async get(userId: number, cardId: number, options: any = {}) {
-        const limiter = this.ApiAdapter.RequestLimitFactory.create("/card", "GET");
+	public async get(userId: number, cardId: number, options: any = {}) {
+		const limiter = this.ApiAdapter.RequestLimitFactory.create("/card", "GET");
 
-        const response = await limiter.run(async () => this.ApiAdapter.get(`/v1/user/${userId}/card/${cardId}`));
+		const response = await limiter.run(async () => this.ApiAdapter.get(`/v1/user/${userId}/card/${cardId}`));
 
-        return response.Response;
-    }
+		return response.Response;
+	}
 
     /**
      * @param {number} userId
      * @param {CardListOptions} options
      * @returns {Promise<void>}
      */
-    public async list(
-        userId: number,
-        options: PaginationOptions = {
-            count: 25,
-            newer_id: false,
-            older_id: false
-        }
-    ) {
-        const params: any = {};
+	public async list(
+		userId: number,
+		options: PaginationOptions = {
+			count: 25,
+			newer_id: false,
+			older_id: false
+		}
+	) {
+		const params: any = {};
 
-        if (options.count !== undefined) {
-            params.count = options.count;
-        }
-        if (options.newer_id !== false && options.newer_id !== undefined) {
-            params.newer_id = options.newer_id;
-        }
-        if (options.older_id !== false && options.older_id !== undefined) {
-            params.older_id = options.older_id;
-        }
+		if (options.count !== undefined) {
+			params.count = options.count;
+		}
+		if (options.newer_id !== false && options.newer_id !== undefined) {
+			params.newer_id = options.newer_id;
+		}
+		if (options.older_id !== false && options.older_id !== undefined) {
+			params.older_id = options.older_id;
+		}
 
-        const limiter = this.ApiAdapter.RequestLimitFactory.create("/card", "LIST");
+		const limiter = this.ApiAdapter.RequestLimitFactory.create("/card", "LIST");
 
-        const response = await limiter.run(async () =>
-            this.ApiAdapter.get(
-                `/v1/user/${userId}/card`,
-                {},
-                {
-                    axiosOptions: {
-                        params: params
-                    }
-                }
-            )
-        );
+		const response = await limiter.run(async () =>
+			this.ApiAdapter.get(
+				`/v1/user/${userId}/card`,
+				{},
+				{
+					axiosOptions: {
+						params: params
+					}
+				}
+			)
+		);
 
-        return response.Response;
-    }
+		return response.Response;
+	}
 
     /**
      * @param {number} userId
@@ -90,41 +90,42 @@ export default class Card implements ApiEndpointInterface {
      * @param options
      * @returns {Promise<any>}
      */
-    public async update(
-        userId: number,
-        cardId: number,
-        pinCode: string = null,
-        activationCode: string = null,
-        status: string = null,
-        cardLimit: Amount = null,
-        limits: LimitCollection = null,
-        //magStripePermission: MagStripePermission = null,
-        countryPermissions: CountryPermissionCollection = null,
-        pinCodeAssignment: PinCodeAssignmentCollection = null,
-        monetaryAccountIdFallback: number = null,
-        options: any = {}
-    ) {
-        const limiter = this.ApiAdapter.RequestLimitFactory.create("/card", "PUT");
+	public async update(
+		userId: number,
+		cardId: number,
+		pinCode: string = null,
+		activationCode: string = null,
+		status: string = null,
+		cardLimit: Amount = null,
+		limits: LimitCollection = null,
+		//magStripePermission: MagStripePermission = null,
+		countryPermissions: CountryPermissionCollection = null,
+		pinCodeAssignment: PinCodeAssignmentCollection = null,
+		monetaryAccountIdFallback: number = null,
+		options: any = {}
+	) {
+		const limiter = this.ApiAdapter.RequestLimitFactory.create("/card", "PUT");
 
-        const response = await limiter.run(async () =>
-            this.ApiAdapter.put(
-                `/v1/user/${userId}/card/${cardId}`,
-                {
-                    pin_code: pinCode,
-                    activation_code: activationCode,
-                    status: status,
-                    card_limit: cardLimit,
-                    limit: limits,
-                    //mag_stripe_permission: magStripePermission,
-                    country_permission: countryPermissions,
-                    pin_code_assignment: pinCodeAssignment,
-                    monetary_account_id_fallback: monetaryAccountIdFallback
-                },
-                {},
-                { isEncrypted: true }
-            )
-        );
+		const response = await limiter.run(async () =>
+			this.ApiAdapter.put(
+				`/v1/user/${userId}/card/${cardId}`,
+				{
+					pin_code: pinCode,
+					activation_code: activationCode,
+					status: status,
+					card_limit: cardLimit,
+					// card_limit_atm field does not update if put and is left out
+					limit: limits,
+					//mag_stripe_permission: magStripePermission, // Depricated?
+					country_permission: countryPermissions,
+					pin_code_assignment: pinCodeAssignment,
+					monetary_account_id_fallback: monetaryAccountIdFallback
+				},
+				{},
+				{ isEncrypted: true }
+			)
+		);
 
-        return response.Response;
-    }
+		return response.Response;
+	}
 }
--- tests/src/Api/Card.test.ts
+++ tests/src/Api/Card.test.ts
@@ -6,49 +6,50 @@ import SetupApp from "../../TestHelpers/SetupApp";
 import { defaultResponse } from "../../TestHelpers/DefaultResponses";
 
 describe("API", () => {
-    beforeEach(() => moxios.install());
-    afterEach(() => moxios.uninstall());
+	beforeEach(() => moxios.install());
+	afterEach(() => moxios.uninstall());
 
-    it("#CARDUPDATE", async () => {
-        const bunqApp: BunqJSClient = await SetupApp();
+	it("#CARDUPDATE", async () => {
+		const bunqApp: BunqJSClient = await SetupApp();
 
-        const request = bunqApp.api.card.update(
-            1,
-            2,
-            "1234",
-            "123456",
-            "ACTIVE",
-            {
-                currency: "EUR",
-                value: "50.00"
-            },
-            [
-                {
-                    daily_limit: "50.00",
-                    currency: "EUR",
-                    type: "CARD_LIMIT_CONTACTLESS"
-                }
-            ],
-            // {
-            // 	expiry_time: null
-            // },
-            [
-                {
-                    country: "NL",
-                    expiry_time: null
-                }
-            ],
-            [
-                {
-                    type: "PRIMARY",
-                    pin_code: "1234",
-                    monetary_account_id: 1234
-                }
-            ]
-        );
-        await defaultResponse(moxios);
-        const response = await request;
+		const request = bunqApp.api.card.update(
+			1,
+			2,
+			"1234",
+			"123456",
+			"ACTIVE",
+			{
+				currency: "EUR",
+				value: "50.00"
+			},
+			[
+				{
+					daily_limit: "50.00",
+					currency: "EUR",
+					type: "CARD_LIMIT_POS_ICC"
+				}
+			],
+			// {
+			// 	expiry_time: null
+			// },
+			[
+				{
+					country: "NL",
+					expiry_time: null
+				}
+			],
+			[
+				{
+					type: "PRIMARY",
+					pin_code: "1334", // Not mandatory
+					monetary_account_id: 1234
+				}
+			],
+			null
+		);
+		await defaultResponse(moxios);
+		const response = await request;
 
-        expect(response).not.toBeNull();
-    });
+		expect(response).not.toBeNull();
+	});
 });
