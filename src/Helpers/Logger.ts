5b96df2a98414b0aca044261b77c92767376167b
Gregory Goijaerts <crecketgaming@gmail.com>
2019-04-05 20:04:54 +0200

    Add new env option to force set loglevel

--- src/Helpers/Logger.ts
+++ src/Helpers/Logger.ts
@@ -3,7 +3,7 @@ declare const require: any;
 const loglevel: any = require("loglevel");
 
 // set logging level based on env
-loglevel.setLevel(process.env.NODE_ENV === "development" ? "trace" : "warn");
+loglevel.setLevel(process.env.BUNQ_JS_CLIENT_LOG_LEVEL || process.env.NODE_ENV === "development" ? "trace" : "warn");
 
 // export the logger
 export default loglevel;
